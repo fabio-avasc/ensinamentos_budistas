f7cf67f13f19943a28430828e716801f8172cd76
Fabio Antunes de Vasconcelos <fabioantunes@brq.com>
2019-11-19 16:12:37 -0300

    Criação do Container Básico

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    title: "Ensinamentos Budistas",
+    home: Container(
+      color: Colors.white,
+    )
+  ));
+
+}
