77b502920a87b74896957d0f423dfb9274cc818a
Fabio Antunes de Vasconcelos <fabioantunes@brq.com>
2019-11-22 18:52:15 -0300

    Lógica das frases e pequenas correções

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -13,6 +15,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _numeroAleatorio = 0;
+  List _frases = [
+    'Guardar raiva é como segurar um carvão em brasa com a intenção de atirá-lo em alguém; é você que se queima.',
+    'Em nossas vidas, a mudança é inevitável. A perda é inevitável. A felicidade reside na nossa adaptabilidade em sobreviver a tudo de ruim.',
+    'Sua tarefa é descobrir o seu trabalho e, então, com todo o coração, dedicar-se a ele.',
+    'Nascemos para morrer, conhecemos pessoas para as deixar e ganhamos coisas para as perder.',
+    'Existem três classes de pessoas que são infelizes: a que não sabe e não pergunta, a que sabe e não ensina e a que ensina e não faz.',
+    'Jamais, em todo o mundo, o ódio acabou com o ódio. O que acaba com o ódio é o amor.'
+  ];
+
+  void _contadorRandom(){
+    setState(() => _numeroAleatorio = Random().nextInt(_frases.length));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,6 +64,17 @@ class _HomeState extends State<Home> {
               color: Colors.deepOrange
             ),
           ),
+          Container(
+            child: Text(
+              _frases[_numeroAleatorio],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.deepOrange,
+                fontSize: 22,
+                fontStyle: FontStyle.italic
+              ),
+            ),
+          ),
           RaisedButton(
             child: Text(
               "Nova Frase",
@@ -57,7 +85,7 @@ class _HomeState extends State<Home> {
               ),
             ) ,
             color: Colors.deepOrange,
-            onPressed: (){},
+            onPressed: _contadorRandom,
           )
         ],
       ),
